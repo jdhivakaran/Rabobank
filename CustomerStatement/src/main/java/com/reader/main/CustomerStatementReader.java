7f1c0433c71ddd9be6acb2773d88709971d3dab5
jdhivakaran <46891719+jdhivakaran@users.noreply.github.com>
2019-12-26 00:54:14 +0530

    Update CustomerStatementReader.java

--- CustomerStatement/src/main/java/com/reader/main/CustomerStatementReader.java
+++ CustomerStatement/src/main/java/com/reader/main/CustomerStatementReader.java
@@ -1,7 +1,6 @@
 package com.reader.main;
 
 import static com.reader.constants.CommonConstants.CSVFORMAT;
-import static com.reader.constants.CommonConstants.FILE_PATH;
 import static com.reader.constants.CommonConstants.XMLFORMAT;
 
 import java.io.File;
@@ -9,6 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 import com.reader.csvreader.OpenCSVReader;
 import com.reader.xmlreader.CustomerParserSAX;
@@ -16,27 +16,38 @@ import com.reader.xmlreader.CustomerParserSAX;
 public class CustomerStatementReader {
  
 	public static void main(String[] args) {
-		try{
-		File folder = new File(FILE_PATH);
+		try(Scanner myObj = new Scanner(System.in)){
+		    System.out.println("Please enter the File Path :");
+		
+		File folder = new File(myObj.nextLine());
 		File[] listOfFiles = folder.listFiles();
 		Path path = null;
 		String contentType = null;
+		boolean validFileExist =false;
+		if(listOfFiles.length==0) {
+			System.out.println("No File exist in this folder");
+		}else {
 		for (File file : listOfFiles) {
 		    if (file.isFile()) {
 		    	path = file.toPath(); 
 				contentType = Files.probeContentType(path);
 		    	if(contentType.equalsIgnoreCase(CSVFORMAT)){
 		    		new OpenCSVReader().genrateCSVReport(new FileReader(file));
+		    		validFileExist =true;
 		    	}else if(contentType.equalsIgnoreCase(XMLFORMAT)){
 		    		new CustomerParserSAX().genrateXMLReport(file);
+		    		validFileExist =true;
 		    	}
 		    }
 		} 
+		if(!validFileExist) {
+			System.out.println("No Valid File exist in this folder");
+		}
+		}
 		}catch(IOException e){
 			e.printStackTrace();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
 	}
 }
