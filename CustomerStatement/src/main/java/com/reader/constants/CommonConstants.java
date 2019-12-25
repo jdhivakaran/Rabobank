876781c892a629af7291a5bf2f0c1bab25d6bca6
jdhivakaran <46891719+jdhivakaran@users.noreply.github.com>
2019-12-26 00:54:40 +0530

    Update CommonConstants.java

--- CustomerStatement/src/main/java/com/reader/constants/CommonConstants.java
+++ CustomerStatement/src/main/java/com/reader/constants/CommonConstants.java
@@ -4,5 +4,5 @@ public class CommonConstants {
 
 	public static final String CSVFORMAT ="application/vnd.ms-excel";
 	public static final String XMLFORMAT ="text/xml";
-	public static final String FILE_PATH ="D:/Assignment/BE";
+	//public static final String FILE_PATH ="D:/Assignment/BE";
 }
